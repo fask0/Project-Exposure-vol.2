8223dc61c126a204fa363ed30fe43c3a0510ab93
Fasko <wasilev.8v@gmail.com>
2019-06-17 14:55:17 +0200

    0

---
+++ Project Exposure vol.2/Assets/CameraShake.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake instance;
+
+    private Vector3 _originalPos;
+    private float _timeAtCurrentFrame;
+    private float _timeAtLastFrame;
+    private float _fakeDelta;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        // Calculate a fake delta time, so we can Shake while game is paused.
+        _timeAtCurrentFrame = Time.realtimeSinceStartup;
+        _fakeDelta = _timeAtCurrentFrame - _timeAtLastFrame;
+        _timeAtLastFrame = _timeAtCurrentFrame;
+    }
+
+    public static void Shake(float duration, float amount)
+    {
+        instance._originalPos = instance.gameObject.transform.localPosition;
+        instance.StopAllCoroutines();
+        instance.StartCoroutine(instance.cShake(duration, amount));
+    }
+
+    public IEnumerator cShake(float duration, float amount)
+    {
+        float endTime = Time.time + duration;
+
+        while (duration > 0)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _originalPos + Random.insideUnitSphere * amount, Time.deltaTime * 50);
+
+            duration -= _fakeDelta;
+
+            yield return null;
+        }
+
+        while (transform.localPosition != _originalPos)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _originalPos, Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ Project Exposure vol.2/Assets/StartBehaviourAfterScan.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartBehaviourAfterScan : MonoBehaviour
+{
+    private FishBehaviour fishBehaviour;
+
+    [SerializeField]
+    private bool _startBehaviour = true;
+    [SerializeField]
+    private UnityEvent afterScanEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_startBehaviour)
+            fishBehaviour = GetComponent<FishBehaviour>();
+        if (fishBehaviour != null)
+        {
+            SingleTons.SoundWaveManager.onFishScanEvent += StartFishBehaviour;
+        }
+
+        SingleTons.SoundWaveManager.scanEvents.Add(gameObject, afterScanEvent);
+    }
+
+    private void OnDisable()
+    {
+        SingleTons.SoundWaveManager.onFishScanEvent -= StartFishBehaviour;
+        SingleTons.SoundWaveManager.scanEvents.Remove(gameObject);
+    }
+
+    void StartFishBehaviour(GameObject obj)
+    {
+        if (obj.name == this.name)
+        {
+            fishBehaviour.enabled = true;
+        }
+        else
+        {
+            Debug.Log(obj.name);
+        }
+    }
+}
