d3ad4abfdf0bc4179768959242c01de5852ab831
rpijpker01 <rimme.pijpker@gmail.com>
2019-06-19 09:43:15 +0200

    New terrain and textures and stuff : - )

---
+++ Project Exposure vol.2/Assets/Scripts/SceneLoading/ArtifactParent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ArtifactParent : MonoBehaviour
+{
+    [SerializeField]
+    private List<int> _radarArrowTime = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (_radarArrowTime.Count < transform.childCount)
+            _radarArrowTime.Add(120);
+        while (_radarArrowTime.Count > transform.childCount)
+            _radarArrowTime.RemoveAt(_radarArrowTime.Count - 1);
+    }
+
+    public int GetArrowTime(int index)
+    {
+        return _radarArrowTime[index];
+    }
+}
