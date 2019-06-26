ae2ca84f7d49a996d8bf048ce3fab563159a2631
rpijpker01 <rimme.pijpker@gmail.com>
2019-06-26 15:29:30 +0200

    Conditional Button Behaviour Made It's Perfect Captial Letter : - )

---
+++ Project Exposure vol.2/Assets/Scripts/UI/Menus/Input/ConditionalButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ConditionalButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _button;
+    [SerializeField]
+    private Sprite _imageToSwapTo;
+
+    private TMP_InputField _inputField;
+    private Sprite _originalImage;
+    private Image _imageComponent;
+    private Button _buttonbehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _inputField = GetComponent<TMP_InputField>();
+        _imageComponent = _button.GetComponent<Image>();
+        _originalImage = _imageComponent.sprite;
+        _buttonbehaviour = _button.GetComponent<Button>();
+
+        _imageComponent.sprite = _imageToSwapTo;
+        _buttonbehaviour.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string testString = _inputField.text.Replace(" ", string.Empty);
+        if (testString.Length > 0)
+        {
+            _imageComponent.sprite = _originalImage;
+            _buttonbehaviour.enabled = true;
+        }
+        else
+        {
+            _imageComponent.sprite = _imageToSwapTo;
+            _buttonbehaviour.enabled = false;
+        }
+    }
+}
